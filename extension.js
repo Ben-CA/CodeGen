fcac99acbc0ec7dbbe26c2c290a46f3646614dc5
Benjamin <github@etron.ca>
2019-03-21 15:08:48 -0500

    Boilerplate working

--- extension.js
+++ extension.js
@@ -1,7 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
+const fs = require("fs");
+const path = require("path");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -18,48 +19,57 @@ function activate(context) {
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.CodeGen', function () {
+		  if (!vscode.workspace) {
+			return vscode.window.showErrorMessage(
+			  "Please open a project folder first"
+			);
+		  }
+
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('This is a notification from CodeGen!');
 
 
-		const fs = require("fs");
-		const path = require("path");
 
-		const folderPath = vscode.workspace.workspaceFolders[0].uri
-        .toString()
-        .split(":")[1];
+  
+		const folderPath = vscode.workspace.workspaceFolders[0].uri.toString().split(":")[1];
+  
+		const htmlContent = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8" />
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	  <title>Document</title>
+	  <link rel="stylesheet" href="app.css" />
+  </head>
+  <body>
+	  <script src="app.js"></script>
+  </body>
+  </html>
+		  `;
+  
+		fs.writeFile(path.join(folderPath, "index.html"), htmlContent, err => {
+		  if (err) {
+			return console.log(err);
+		  }
+		  fs.writeFile(path.join(folderPath, "app.js"), "", err => {
+			if (err) {
+			  return console.log(err);
+			}
+			fs.writeFile(path.join(folderPath, "app.css"), "", err => {
+			  if (err) {
+				return console.log(err);
+			  }
+			  vscode.window.showWarningMessage("Created base files!");
+			});
+		  });
+		});
+	
 
-      const htmlContent = `<!DOCTYPE html>
-							<html lang="en">
-							<head>
-								<meta charset="UTF-8" />
-								<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-								<meta http-equiv="X-UA-Compatible" content="ie=edge" />
-								<title>Document</title>
-								<link rel="stylesheet" href="app.css" />
-							</head>
-							<body>
-								<script src="app.js"></script>
-							</body>
-							</html>`;
 
-      fs.writeFile(path.join(folderPath, "index.html"), htmlContent, err => {
-        if (err) {
-          return vscode.window.showErrorMessage(
-            "Failed to create boilerplate file!"
-          );
-        }
-        vscode.window.showInformationMessage("Created boilerplate files");
-      });
-    
-
-
-
-
-
-
+	vscode.window.setStatusBarMessage('Completed', 5000);
 
 // end of code for running this function
 	});
