8912fb63dd5ccde51ece50c9562cc7c20be729b0
Benjamin <github@etron.ca>
2019-03-22 10:16:27 -0500

    Testing

--- extension.js
+++ extension.js
@@ -1,74 +1,97 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const fs = require("fs");
-const path = require("path");
-
-let doIT;
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
+let iCG;
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-			// On Acivate, add 'doIT' status bar button
-			doIT = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 2);
-			doIT.text = 'doIT';
-			doIT.command = `extension.CodeGen`;
-			doIT.tooltip = 'doIT on current file';
-			doIT.show();
+			// On Activation, add status bar button to call function - sets up the Webview Panel
+			iCG = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 2);
+			iCG.text = '$(rocket) CodeGen';
+			iCG.command = `extension.CodeGen`;
+			iCG.tooltip = 'Open CodeGen tab';
+			iCG.show();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "CodeGen" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.CodeGen', function () {
-		  if (!vscode.workspace) {
-			return vscode.window.showErrorMessage(
-			  "Please open a project folder first"
-			);
-		  }
 
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('This is a notification from CodeGen!');
+
+			// Create and show panel
+			const panel = vscode.window.createWebviewPanel('default', "Code Generator", vscode.ViewColumn.One, {
+				enableScripts: true
+			  });
+			// And set its HTML content
+			panel.webview.html = getWebviewContent();
+		
+			function getWebviewContent() {
+				return `<!DOCTYPE html>
+				<html lang="en">
+				<head>
+				<!-- Copyright 2019 etron.ca. All rights reserved. -->
+				<title>CodeGen</title>
+				<meta charset="UTF-8">
+				<script>
+				function DO()
+				{
+					var Results = document.getElementById('Result');
+					Results.innerHTML = ''; // Blank out previous values in Result field
+				
+					var p = document.createElement('h4');
+					p.innerHTML = 'Results:'; // insert results heading
+					Results.append(p);
+				
+					var codeline = document.getElementById('Code').value; // Get line of code
+					var valuestouse = document.getElementById('Values').value; // Get values to use
+					// Turn values from CSV into array
+					var array = valuestouse.split(',');
+					// For each item in array
+					var arrayLength = array.length;
+					for (var i = 0; i < arrayLength; i++) {
+					// Make the the replacement
+					var newline = codeline.replace(/%%%/g, array[i]);
+					// Append the result, and repeat for each line
+					var el = document.createElement('code');
+					el.innerHTML = '<xmp>' + newline + '</xmp>';
+					Results.append(el);
+					}
+					document.getElementById('Count').innerHTML = "<h5><i>Generated " + arrayLength + " lines of code.</i></h5>";
+					}
+				
+				</script>
+
+				</head>
+				<body>
+				<br>
+				<h1>CodeGen</h1>
+				<br>
+				Input line of code: <span style="font-size: 75%; color: #666666;">(put %%% wherever you want the variable entered)</span>
+				<br><br>
+				<input type="text" ID="Code" size="151" value="This line of code contains %%% and %%% again." />
+				<br><br>
+				Input values, separated by commas: <span style="font-size: 75%; color: #666666;">(use search & replace to distill data to CSV values)</span>
+				<br><br>
+				<textarea ID="Values" cols="150" rows="10">valueA,valueB,valueC</textarea>
+				<br><br>
+				<button onclick="DO();">Generate</button>
+				<br><br>
+				<div ID="Result"></div>
+				<br><br>
+				<span ID="Count"></span>
+				<br><br>
+				</body>
+				</html>`;
+			}
 
 
 
-  
-		const folderPath = vscode.workspace.workspaceFolders[0].uri.toString().split(":")[1];
-  
-		const htmlContent = `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8" />
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-	  <title>Document</title>
-	  <link rel="stylesheet" href="app.css" />
-  </head>
-  <body>
-	  <script src="app.js"></script>
-  </body>
-  </html>
-		  `;
-  
-		fs.writeFile(path.join(folderPath, "index.html"), htmlContent, err => {
-		  if (err) {
-			return console.log(err);
-		  }
-		});
-	
 
-// vscode.window.showWarningMessage("Created base files!");
-	vscode.window.setStatusBarMessage('Completed', 5000);
+// Display a message box to the user
+// vscode.window.showInformationMessage('Done');
+// vscode.window.showWarningMessage("Done!");
+// vscode.window.setStatusBarMessage('Done', 5000);
 
 // end of code for running this function
 	});
