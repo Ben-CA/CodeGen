93844367d8962da05f8290179419c050325f22b1
Benjamin <github@etron.ca>
2019-03-22 10:31:56 -0500

    Updates

--- extension.js
+++ extension.js
@@ -17,8 +17,7 @@ function activate(context) {
 	let disposable = vscode.commands.registerCommand('extension.CodeGen', function () {
 
 		// The code you place here will be executed every time your command is executed
-
-
+		
 			// Create and show panel
 			const panel = vscode.window.createWebviewPanel('default', "Code Generator", vscode.ViewColumn.One, {
 				enableScripts: true
@@ -34,15 +33,12 @@ function activate(context) {
 				<title>CodeGen</title>
 				<meta charset="UTF-8">
 				<script>
-				function DO()
-				{
+				function DO() {
 					var Results = document.getElementById('Result');
 					Results.innerHTML = ''; // Blank out previous values in Result field
-				
 					var p = document.createElement('h4');
 					p.innerHTML = 'Results:'; // insert results heading
 					Results.append(p);
-				
 					var codeline = document.getElementById('Code').value; // Get line of code
 					var valuestouse = document.getElementById('Values').value; // Get values to use
 					// Turn values from CSV into array
@@ -57,41 +53,36 @@ function activate(context) {
 					el.innerHTML = '<xmp>' + newline + '</xmp>';
 					Results.append(el);
 					}
-					document.getElementById('Count').innerHTML = "<h5><i>Generated " + arrayLength + " lines of code.</i></h5>";
+					document.getElementById('Count').innerHTML = "Generated " + arrayLength + " lines of code.";
 					}
-				
 				</script>
-
+				
 				</head>
-				<body>
-				<br>
-				<h1>CodeGen</h1>
-				<br>
-				Input line of code: <span style="font-size: 75%; color: #666666;">(put %%% wherever you want the variable entered)</span>
-				<br><br>
-				<input type="text" ID="Code" size="151" value="This line of code contains %%% and %%% again." />
-				<br><br>
-				Input values, separated by commas: <span style="font-size: 75%; color: #666666;">(use search & replace to distill data to CSV values)</span>
-				<br><br>
-				<textarea ID="Values" cols="150" rows="10">valueA,valueB,valueC</textarea>
-				<br><br>
-				<button onclick="DO();">Generate</button>
-				<br><br>
-				<div ID="Result"></div>
-				<br><br>
-				<span ID="Count"></span>
-				<br><br>
+				<body style="font-family: sans-serif">
+					<div style="width: 100%">
+						<div style="width: 80%; margin-left: auto; margin-right: auto;">
+							<form action="javascript:DO();">
+								<h1>CodeGen</h1>
+								<div style="padding-top: 5px; padding-bottom: 10px; font-size: 16px; font-weight: bold;">
+								Input line of code: <span style="float: right; font-size: 12px; color: #666666;">(put %%% wherever you want the variable entered)</span>
+								</div>
+								<input type="text" ID="Code" style="width: 100%; padding: 10px; font-size: 14px; border-radius: 3px; border: 1px #000000 solid;" value="This line of 'code' contains %%% and %%% again." placeholder="Use %%%" />
+								<div style="padding-top: 30px; padding-bottom: 10px; font-size: 16px; font-weight: bold;">
+								Input values, separated by commas: <span style="float: right; font-size: 12px; color: #666666;">(use search & replace to distill data to CSV values)</span>
+								</div>
+								<textarea ID="Values" rows="8" style="width: 100%; padding: 10px; font-size: 14px; border-radius: 3px; border: 1px #000000 solid;" placeholder="Enter CSV values">valueA,valueB,valueC</textarea>
+				
+								<div style="text-align: center; padding-top: 15px;">
+								<button type="submit" style="width: 300px; padding-top: 10px; padding-bottom: 10px; font-size: 16px; font-weight: bold; border-radius: 8px;">Generate</button></div>
+								
+								<div ID="Result" style="padding-top: 5px"></div>
+								<div ID="Count" style="padding-top: 15px; font-style: italic; font-weight: bold; font-size: 12px;"></div>
+							</form>
+						</div>
+					</div>
 				</body>
 				</html>`;
 			}
-
-
-
-
-// Display a message box to the user
-// vscode.window.showInformationMessage('Done');
-// vscode.window.showWarningMessage("Done!");
-// vscode.window.setStatusBarMessage('Done', 5000);
 
 // end of code for running this function
 	});
